c1d052957c30754c26578cd09a64e4df6da0a4b4
rachel <rachelg600@gmail.com>
2023-11-28 16:05:56 +0800

    edits

--- public/table.js
+++ public/table.js
@@ -2,90 +2,103 @@ let itemList = [];
 let dateList = [];
 
 function initialize() {
+    // Show today's date
     displayCurrentDate();
+    // Load saved items from localStorage
+    itemList = JSON.parse(localStorage.getItem('itemList')) || [];
+    dateList = JSON.parse(localStorage.getItem('dateList')) || [];
+    displaySavedItems();
 }
 
+// Create table
+function createRow(item, user, date) {
+  let tableBody = document.getElementById("table-body");
+  let row = tableBody.insertRow();
+  let cellItem = row.insertCell(0);
+  let cellUser = row.insertCell(1);
+  let cellDate = row.insertCell(2);
+  let cellAction = row.insertCell(3);
+
+  cellItem.innerHTML = item;
+  cellUser.innerHTML = user;
+  cellDate.innerHTML = date;
+
+  return { row, cellItem, cellUser, cellDate, cellAction };
+}
+
+// Add item to table
 function addItem() {
   let item = document.getElementById("item").value;
   let selectedUser = userSelect.options[userSelect.selectedIndex].value;
   let currentDate = new Date();
   let formattedDate = currentDate.toISOString().split('T')[0];
-  
-  // let userSelect = document.getElementById("userSelect");
-  // let date = document.getElementById("date").value;
 
-if (item.trim() !== "") {
-      //create table
-      let tableBody = document.getElementById("table-body");
-      
-      // Create a new row
-      let row = tableBody.insertRow();
+  if (item.trim() !== "") {
+      let { row, cellItem, cellUser, cellDate, cellAction } = createRow(item, selectedUser, formattedDate);
 
-      // Create cells for item and date added
-      let cellItem = row.insertCell(0);
-      let cellUser = row.insertCell(1);
-      let cellDate = row.insertCell(2);
-      let cellAction = row.insertCell(3);
-
-      // Set the content of cells
-      cellItem.innerHTML = item;
-      cellUser.innerHTML = selectedUser;
-      cellDate.innerHTML = formattedDate;
-
-      // Push item and date to arrays
-      itemList.push(item);
-      dateList.push(formattedDate);
-    
-      // Track strike-off status
-      let isStrikedOff = false;
-
-      // Allow strike off of items no longer needed/bought
-      cellItem.addEventListener("click", function() {
-        if (!isStrikedOff) {
-          // Apply the strike-off style to the item
-          cellItem.style.textDecoration = "line-through";
-          cellUser.style.textDecoration = "line-through";
-          cellDate.style.textDecoration = "line-through";
-          isStrikedOff = true;
-          } else {
-                // Remove the strike-off style
-                cellItem.style.textDecoration = "none";
-                cellUser.style.textDecoration = "none";
-                cellDate.style.textDecoration = "none";
-                isStrikedOff = false;
-            }
+      // Strike off item when clicked
+      cellItem.addEventListener("click", function () {
+        toggleStrikeOff(cellItem, cellUser, cellDate);
       });
 
-      // Remove items from list
+      // Remove item when "X" button is clicked
       let removeButton = document.createElement("button");
       removeButton.appendChild(document.createTextNode("X"));
-      // Append the remove button to the cell
       cellAction.appendChild(removeButton);
 
-      removeButton.addEventListener("click", function() {
-        // Prompt the user for confirmation before removing the item
-        const confirmRemove = confirm("Remove this item from list?");
-        
-        if (confirmRemove) {
-            // Remove the row from the table
-            row.remove();
-
-            // Optionally, remove the item from your arrays
-            const index = itemList.indexOf(item);
-            if (index !== -1) {
-                itemList.splice(index, 1);
-                dateList.splice(index, 1);
-            }
-        }
+      removeButton.addEventListener("click", function () {
+          removeItem(row, item, formattedDate);
       });
 
-      // Clear input fields
       document.getElementById("item").value = "";
       document.getElementById("user").value = "";
-  }
-  else {
-      // Display an error message for invalid date
+  } else {
       alert("Please enter an item description.");
+  }
+}
+
+// Toggle strike off
+function toggleStrikeOff(cellItem, cellUser, cellDate) {
+  cellItem.classList.toggle("strikeoff");
+  cellUser.classList.toggle("strikeoff");
+  cellDate.classList.toggle("strikeoff");
+}
+
+// Remove item from table
+function removeItem(row, item, formattedDate) {
+  const confirmRemove = confirm("Remove this item from the list?");
+  if (confirmRemove) {
+      row.remove();
+      const index = itemList.indexOf(item);
+      if (index !== -1) {
+          itemList.splice(index, 1);
+          dateList.splice(index, 1);
+          localStorage.setItem('itemList', JSON.stringify(itemList));
+          localStorage.setItem('dateList', JSON.stringify(dateList));
+          displaySavedItems();  // Refresh display after removal
+      }
+  }
+}
+
+// Display saved list when page loads
+function displaySavedItems() {
+  let tableBody = document.getElementById("table-body");
+  for (let i = 0; i < itemList.length; i++) {
+      let { row, cellItem, cellUser, cellDate, cellAction } = createRow(itemList[i], "User", dateList[i]);
+
+      // Strike off item when clicked
+      cellItem.addEventListener("click", function () {
+        toggleStrikeOff(cellItem, cellUser, cellDate);
+      });
+
+      // Remove item when "X" button is clicked
+      let removeButton = document.createElement("button");
+      removeButton.appendChild(document.createTextNode("X"));
+      cellAction.appendChild(removeButton);
+
+      removeButton.addEventListener("click", function () {
+          removeItem(row, itemList[i], dateList[i]);
+      });
   }
 }
 
@@ -129,6 +142,8 @@ function clearList() {
     // Clear the arrays
     itemList = [];
     dateList = [];
+    localStorage.removeItem('itemList');
+    localStorage.removeItem('dateList');
 }
 
 // Display current date
@@ -139,5 +154,6 @@ let formattedDate = currentDate.toLocaleDateString('en-US', options);
 document.getElementById('current-date').innerText = "as of " + formattedDate;
 }
 
-// Call date function on page load
+// Call date function and saved items on page load
 displayCurrentDate();
+displaySavedItems();
